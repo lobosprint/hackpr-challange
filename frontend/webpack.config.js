5fb3d80468ecefb755066ddbd4fb86be63529b44
Khalah Jones-Golden <khasan222@gmail.com>
2016-10-15 19:04:18 -0400

    [Bootstrap] Added bootstrap to the frontend

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   entry: './src/components/app.jsx',
   output: {
-    filename: 'public/app.bundle.js'
+    filename: 'app.bundle.js',
+    path: './public',
+    publicPath: '/public/'
   },
   resolve: {
     extensions: ['', '.js', '.jsx']
@@ -18,6 +20,30 @@ module.exports = {
       query: {
         presets: ['es2016', 'react']
       }
+    }, {
+      test: /\.(woff|woff2)$/,
+      loader: 'url-loader?limit=100000&mimetype=application/font-woff'
+    }, {
+      test: /\.ttf$/,
+      loader: 'file-loader'
+    }, {
+      test: /\.eot$/,
+      loader: 'file-loader'
+    }, {
+      test: /\.svg$/,
+      loader: 'file-loader'
+    }, {
+      test: /\.css$/,
+      loader: 'style-loader!css-loader'
+    }, {
+      test: /\.less$/,
+      loader: 'style!css!less'
+    }, {
+      test: /\.(jpe?g|png|gif|svg)$/i,
+      loaders: [
+        'file?hash=sha512&digest=hex&name=[hash].[ext]',
+        'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+      ]
     }]
   }
 }
